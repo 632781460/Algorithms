7a90608f5f1314b1c315f59bd2c7c835694a0a5a
632781460 <song_kaiyu@163.com>
2013-03-25 21:52:45 +0800

    Initial commit

---
+++ Algorithms/SortHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    public class SortHelper
+    {
+        public static void InsertionSort<T>(IList<T> source,bool aesc)
+			where T:IComparable<T>
+        {
+            int length = source.Count,
+                compareValue = aesc ? 1:-1;
+			T key = default(T);
+            if (length > 1) {
+                for (int i = 1; i < length; i++)
+                {
+                    key = source[i];
+                    int ii = i - 1;
+                    for (; ii >= 0 && source[ii].CompareTo(key) == compareValue; ii--)
+                    {
+                        source[ii + 1] = source[ii];
+                    }
+                    source[ii+1] = key;
+                }
+            }
+        }
+
+        public static void MergeSort<T>(IList<T> source, int begin, int end, bool aesc)
+			where T:IComparable<T>
+        {
+            if (begin < end)
+            {
+                int mid = (begin + end) / 2;
+                MergeSort<T>(source,begin, mid,aesc);
+                MergeSort<T>(source, mid+1, end, aesc);
+                Merge<T>(source, begin, mid, end, aesc);
+            }
+        }
+
+        private static void Merge<T>(IList<T> source, int p1, int p1p2, int p2p3, bool aesc)
+			where T:IComparable<T>
+        {
+            int a1Count = p1p2 - p1 + 1,
+                a2Count = p2p3 - p1p2,
+                li=0,
+                ri=0,
+				compareValue=aesc?-1:1,
+                length=a1Count+a2Count;
+            T[] a1 = new T[a1Count];
+            T[] a2 = new T[a2Count];
+            for (int i = 0; i < a1Count; i++)
+            {
+                a1[i] = source[p1+i];
+            }
+            for (int i = 0; i < a2Count; i++)
+            {
+                a2[i] = source[p1p2 + i+1]; 
+            }
+            for (int i = p1; i < p2p3+1; i++)
+            {
+                if ( ri>=a2Count || 
+                    (li < a1Count  && a1[li].CompareTo(a2[ri]) == compareValue )
+                   )
+                {
+                    source[i] = a1[li];
+                    ++li;
+                }
+                else {
+                    source[i] = a2[ri];
+                    ++ri;
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsUnitTest/AlgrithomTester.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Algorithms;
+using System.Diagnostics;
+namespace AlgrithomUnitTest
+{
+    [TestFixture]
+    public class AlgrithomTester
+    {
+
+        public int[] m_ints;
+
+        private int LOOPCOUNT = 10000000;
+
+        public AlgrithomTester()
+        {
+            m_ints = new int[LOOPCOUNT]; 
+        }
+        [Test]
+        public void TestInsertionTest()
+        {
+            int[] ints = new int[]{3, 9, 1, 4, 0};
+            SortHelper.InsertionSort(ints, true);
+            Assert.AreEqual(0, ints[0]);
+            Assert.AreEqual(1, ints[1]);
+            Assert.AreEqual(3, ints[2]);
+            Assert.AreEqual(4, ints[3]);
+            Assert.AreEqual(9, ints[4]);
+            SortHelper.InsertionSort(ints, false);
+			Assert.AreEqual(0, ints[4]);
+            Assert.AreEqual(1, ints[3]);
+            Assert.AreEqual(3, ints[2]);
+            Assert.AreEqual(4, ints[1]);
+            Assert.AreEqual(9, ints[0]);
+            ints = new int[] { 1 };
+            SortHelper.InsertionSort(ints, true);
+            Assert.AreEqual(1, ints[0]);
+            ints = new int[] { 2, 1 };
+            SortHelper.InsertionSort(ints, true);
+            Assert.AreEqual(1, ints[0]);
+            Assert.AreEqual(2, ints[1]);
+            ints = new int[] { 10, 10 };
+            SortHelper.InsertionSort(ints, false);
+            Assert.AreEqual(10, ints[0]);
+            Assert.AreEqual(10, ints[1]);
+            for (int i = 10, j = 0; i > 0; i--)
+            {
+                m_ints[j++] = i;
+            }
+            SortHelper.InsertionSort(m_ints, true);
+        }
+
+        [Test]
+        public void TestMergeSort()
+        { 
+			int [] ints = new int[]{10,9,8,7,45,641,134,5515};
+            SortHelper.MergeSort(ints, 0, ints.Length - 1,true);
+            Assert.AreEqual(7, ints[0]);
+            Assert.AreEqual(8, ints[1]);
+            Assert.AreEqual(9, ints[2]);
+            Assert.AreEqual(10, ints[3]);
+            SortHelper.MergeSort(ints, 0, ints.Length - 1, false);
+            Assert.AreEqual(5515, ints[0]);
+            ints = new int[] { 2, 1 };
+            SortHelper.MergeSort(ints, 0, 1, true);
+			Assert.AreEqual(1,ints[0]);
+			Assert.AreEqual(2,ints[1]);
+            ints = new int[] { 1 };
+            Assert.AreEqual(1, ints[0]);
+            for (int i = LOOPCOUNT, j = 0; i > 0; i--)
+            {
+                m_ints[j++] = i;
+            }
+            SortHelper.MergeSort(m_ints, 0, LOOPCOUNT - 1, true);
+        }
+    }
+} 
